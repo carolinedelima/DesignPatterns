2b9bc690a52756b2dd9ab9935cc5a21c5969dcad
Caroline de Lima <cdlima@daitan.com>
2020-09-04 13:53:47 -0300

    Upload Facade

---
+++ Facade/AccountNumberCheck.java
@@ -0,0 +1,20 @@
+public class AccountNumberCheck {
+
+    private int accountNumber = 12345678;
+
+    public int getAccountNumber() { return accountNumber; }
+
+    public boolean accountActive(int accNumToCheck) {
+
+        if (accNumToCheck == getAccountNumber()) {
+
+            return true;
+
+        } else {
+
+            return false;
+
+        }
+    }
+
+}
