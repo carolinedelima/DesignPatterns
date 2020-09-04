9fef8724ca3b301f4c647ac537ebe0b4e6ea5119
Caroline de Lima <cdlima@daitan.com>
2020-09-04 15:46:30 -0300

    Upload FactoryMethod

---
+++ FactoryMethod/JpegReader.java
@@ -0,0 +1,14 @@
+public class JpegReader implements ImageReader {
+
+    private DecodedImage decodedImage;
+
+    public JpegReader(String image) {
+        decodedImage = new DecodedImage(image);
+    }
+
+    @Override
+    public DecodedImage getDecodeImage() {
+        return decodedImage;
+    }
+    
+}
